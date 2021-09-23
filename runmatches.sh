441ad20b110ea21d65f7c51ef53e220bd66d6838
Aaron Gullickson <aarongullickson@gmail.com>
2021-09-23 15:05:46 -0700

    Add shell script for running entire linkage
    A few of the final programs still need to be integrated and are 
    commented out. I also still need to add comments to this file.

---
+++ runmatches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -r output/*
+
+perl m2b.pl
+perl b2m.pl
+perl m2m.pl
+perl b2d.pl
+perl create_initial_datasets.pl
+perl correctremar.pl
+perl p2d.pl
+perl m2d.pl
+perl lastevent.pl
+#perl g2p.pl
+#perl addgptoloe.pl
+#perl getkin.pl
+#perl builddesignmatrix.pl
