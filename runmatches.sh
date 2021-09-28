2ee3530f7717b99f8906d4bee9d2baadeff12e04
Aaron Gullickson <aarongullickson@gmail.com>
2021-09-28 12:00:23 -0700

    Add comments to runmatches.sh

--- runmatches.sh
+++ runmatches.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# This script will run the entire matching routine in Perl. The matches are 
+# output as tab separated text files in the output directory and diagnostic
+# reports are written to output/diagnostics. 
+
+# First remove all prior output
 rm -r output/*
 mkdir output/diagnostics/
 
+# do the initial matching to create full dataset
 perl m2b.pl
 perl b2m.pl
 perl m2m.pl
 perl b2d.pl
 perl create_initial_dataset.pl
+
+# correct pointers for remarriages
 perl correctremar.pl
+
+# link people without birth records to death records
 perl p2d.pl
+
+#  add the marriage to death hand links
 perl m2d.pl
+
+# match godparents and marriage witnesses to people
 perl g2p.pl
+
+# add the last event for the person, including godparentage and marriage
+# witness
 perl lastevent.pl
